9fa2e39eef0ec79a528a8be47fda614bf29795a2
vulcan9 <pdi1066@naver.com>
2016-07-12 17:02:03 +0900

    Jquery 라이브러리 의존성 없앰

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
 # BeatPattern
 스크린 터치 (또는 마우스) 에 반응하여 두드리는 시간 간격(리듬 패턴)의 유사성을 검사하는 라이브러리
+
+	* 이름 : JavaScript-BeatPattern
+	* Version : 1.0.5 (2016.07.12)
+	* (c) Dong-il Park(vulcanProject : http://vulcan9.tistory.com)
+	*
+	* Dependancy :
+	* 		- None
+	*
+	* 용도 : 스크린 터치 (또는 마우스) 에 반응하여 두드리는 시간 간격(리듬 패턴)의 유사성을 검사하는 라이브러리
+	*        사용자 인증하는 방법으로 사용할 수 있다.
+	*        개인적으로는 프로그램 Easter Egg 런칭하는 방법으로 사용하고 있다.
+	*
+	* 브라우져 지원 여부 : all browsers, except IE before version 10
+	*
+	* 사용방법 : 별도 문서 (test.html) 참고
+
+# API 목록
+
+#### constant
+	* MIN_SHORT : Number - 짧게 누름 시간 간격 구간 (10~220, 220이하), 10 milisecond
+	* MAX_SHORT : Number - 짧게 누름 시간 간격 구간 (10~220, 220이하), 220 milisecond
+	* MIN_LONG : Number - 보통 누름 시간 간격 구간 (220 ~ 900 이하), 220 milisecond
+	* MAX_LONG : Number - 보통 누름 시간 간격 구간 (220 ~ 900 이하), 900 milisecond
+	* MIN_VARY_LONG : Number - 길게 누름 시간 간격 구간 (900 ~ 2500 이하), 900 milisecond
+	* MAX_VARY_LONG : Number - 길게 누름 시간 간격 구간 (900 ~ 2500 이하), 2500 milisecond
+	* MAX_INTERVAL : Number - 누름 간격 보류 시간 허용치 (시간안에 다음 누름이 인식되어야 함), 2500 milisecond
+	* RETRY_INTERVAL : Number - 패턴 실패시 다음 검사 가능 시점까지 시간 간격, 100 milisecond
+
+#### properties
+	*
+
+#### Function
+	* createPattern : function(target) - 누름을 인식할 target을 설정함
+	* setPattern : function(patternData, onSuccess, onFail) - 패턴 데이터를 입력하고 핸들러를 등록함
+	* hasPattern : function(dataObj) - 패턴이 이미 등록되어 있는지 검사
+	* resetPattern : function() - 기존 패턴과 이벤트로 다시 검사 초기화
+	* destroy : function(anchorID) - 모두 초기화
+
+# 샘플 코드
+
+```
+        var pattern = new BeatPattern();
+
+        // 이벤트 세팅
+        pattern.createPattern(document.body);
+
+        // 패턴 세팅
+        var patternData = [0, 0, 0, 1, 1, 1];
+        pattern.setPattern(patternData, onSuccess, onFail);
+
+        function onSuccess(){}
+        function onFail(){}
+
+        // 기존 패턴과 이벤트로 다시 검사 초기화
+        pattern.resetPattern();
+```
+패턴 데이터를 정의할때는 0,1,2 값으로 표기할 수 있다
+ - 0 : 짧게 누르고 있는 상태
+ - 1 : 중간 누르고 있는 상태
+ - 2 : 길게 누르고 있는 상태
+각 값들은 누르는 행동 사이 시간 간격이 아니라 누르고 있는 상태의 시간 간격을 의미한다.
+따라서 위 샘플에서 사용한 패턴은 짧게 3번 누른 후 중간 길이로 각 3번 눌렀을때 패턴일치 결과를 리턴한다.
+
+# History
+
+##### Version : 1.0.0 (2014년 어느때...)
+		- Actionscript 코드로 첫 배포
+##### Version : 1.0.3 (2015년 어느때...)
+		- javascript 코드로 포팅
+##### Version : 1.0.5 (2016.07.12)
+		- Jquery 라이브러리 의존성 없앰
+		- Github 배포
---
+++ src/BeatPattern.js
@@ -0,0 +1,337 @@
+/**
+ * Created by dongil on 2016-07-12.
+ */
+
+
+/**
+ var pattern = new BeatPattern();
+
+ // 이벤트 세팅
+ pattern.createPattern(this);
+
+ // 패턴 세팅
+ var patternData = [0, 0, 0, 1, 1, 1];
+ pattern.setPattern(patternData, onSuccess, onFail);
+
+ function onSuccess(){}
+ function onFail(){}
+
+ // 기존 패턴과 이벤트로 다시 검사 초기화
+ pattern.resetPattern();
+*/
+
+var out = function(){
+    //console.log.apply(console, arguments);
+};
+
+function BeatPattern(){}
+
+BeatPattern.prototype = {
+    _currentPattern: undefined,
+
+    _onSuccess: undefined,
+    _onFail: undefined,
+
+    setPattern: function(patternArray, onSuccess, onFail){
+        this._currentPattern = patternArray;
+        this._onSuccess = onSuccess;
+        this._onFail = onFail;
+
+        if(this.hasPattern){
+            out("패턴검사 가능");
+        }else{
+            out("패턴이 정의되지 않았습니다.");
+        }
+    },
+
+    hasPattern: function (){
+        return (this._currentPattern && this._currentPattern.length > 1);
+    },
+
+    resetPattern: function (target, patternArray){
+        target = target || this.target;
+        patternArray = patternArray || this._currentPattern;
+
+        this.clear();
+        this.deletePattern();
+
+        this.createPattern(target);
+        this.setPattern(patternArray);
+    },
+
+    destroy: function(){
+        this.clear();
+        if(this.deletePattern) this.deletePattern();
+        this._currentPattern = null;
+        this._onSuccess = null;
+        this._onFail = null;
+    },
+
+    ////////////////////////////////////////////////////////////////
+    //
+    // Pattern 인식 이벤트 객체
+    //
+    ////////////////////////////////////////////////////////////////
+
+    // 마우스 이벤트를 받을 객체
+    target: undefined,
+
+    // 마우스 누름 시간 간격을 인식하여 올바른 패턴과 비교한다
+    deletePattern: undefined,
+
+    createPattern: function (target){
+        if(!target) return;
+        var self = this;
+
+        this.deletePattern = function (){
+            if(!self.target) return;
+            //$(target).off('mousedown', onMouseDown);
+            //$(target).off('mouseup', onMouseUp);
+            self._off(target, 'mousedown', onMouseDown);
+            self._off(target, 'mouseup', onMouseUp);
+            self.target = null;
+        };
+
+        //$(target).on('mousedown', onMouseDown);
+        //$(target).on('mouseup', onMouseUp);
+        this._on(target, 'mousedown', onMouseDown);
+        this._on(target, 'mouseup', onMouseUp);
+        this.target = target;
+
+        function onMouseDown(){
+            // 시간 간격 체크
+            self.startGesture();
+        }
+        function onMouseUp(){
+            // 시간 간격 체크
+            self.endGesture();
+        }
+    },
+
+    _on: function(target, eventType, handler){
+        if (target.addEventListener){
+            // 웹 표준 브라우저용 이벤트 등록
+            target.addEventListener(eventType, handler, false);
+        } else if (target.attachEvent) {
+            // IE용 이벤트 등록
+            target.attachEvent('on' + eventType, handler);
+        } else if (target['on' + eventType]) {
+            // DOM 레벨 0
+            target['on' + eventType] = handler;
+        }
+    },
+    _off: function(target, eventType, listener){
+        if (target.removeEventListener){
+            // 웹 표준 브라우저용 이벤트 제거
+            target.removeEventListener(eventType, listener);
+        }else if (target.detachEvent) {
+            // IE용 이벤트 제거
+            target.detachEvent('on' + eventType, listener);
+        } else if (target['on' + eventType]) {
+            // DOM 레벨 0
+            target['on' + eventType] = null;
+        }
+    },
+
+    /////////////////////////////////
+    // 체크
+    /////////////////////////////////
+
+    // 짧게 누름 시간 간격 구간 (10~220, 220이하)
+    MIN_SHORT: 10,
+    MAX_SHORT: 220,
+
+    // 보통 누름 시간 간격 구간 (220 ~ 900 이하)
+    MIN_LONG: 220,
+    MAX_LONG: 900,
+
+    // 길게 누름 시간 간격 구간 (900 ~ 2500 이하)
+    MIN_VARY_LONG: 900,
+    MAX_VARY_LONG: 2500,
+
+    // pattern 간격 보류 시간 허용치
+    // INTERVAL 시간안에 다음 패턴이 검사되어야 한다
+    //MAX_INTERVAL: 900,
+    MAX_INTERVAL: 2500,
+
+    // 패턴 실패시 다음 검사 가능 시점까지 인터벌
+    RETRY_INTERVAL: 100,
+
+    //-------------------------------
+    // 시간 체크
+    //-------------------------------
+
+    _sTime: 0,
+    _eTime: 0,
+
+    getTimer: function(){
+        return (new Date()).getTime();
+    },
+
+    startGesture: function (){
+        this.clearNextInterval();
+        this._sTime = this.getTimer();
+    },
+
+    endGesture: function (){
+        this._eTime = this.getTimer();
+        this.checkProgress();
+    },
+
+    // gesture 지속 시간 (마우스 누르고 있는 시간)
+    getGestureDuration: function (){
+        //duration
+        return this._eTime - this._sTime;
+    },
+
+    //-------------------------------
+    // 패턴 체크
+    //-------------------------------
+
+    _gestureIndex: -1,
+
+    checkProgress: function (){
+        if(!this.hasPattern){
+            out("패턴이 정의되지 않았습니다.");
+            return;
+        }
+
+        if(this._gestureIndex < 0){
+            //******************************
+            // 패턴 초기화
+
+            //this.setPattern(_patterns[0]);
+
+            //******************************
+
+            out("패턴 비교 시작");
+        }
+
+        ++this._gestureIndex;
+
+        // 합격 시간 구간
+        var signal = this._currentPattern[this._gestureIndex];
+        var minimum;
+        var maximum;
+        switch(signal){
+            case 0:
+                minimum = this.MIN_SHORT;
+                maximum = this.MAX_SHORT;
+                break;
+            case 1:
+                minimum = this.MIN_LONG;
+                maximum = this.MAX_LONG;
+                break;
+            case 2:
+                minimum = this.MIN_VARY_LONG;
+                maximum = this.MAX_VARY_LONG;
+                break;
+        }
+
+        // 실제 걸린 시간
+        var delay = this.getGestureDuration();
+        out("delay : ", delay);
+
+        if(minimum > delay || maximum < delay){
+            // 실패
+            this.patternFailed();
+            return;
+        }
+
+        // 통과
+        if(this._gestureIndex == this._currentPattern.length-1){
+            // 모든 패턴을 통과했으므로 종료
+            this.patternComplete();
+            return;
+        }
+
+        // 다음 패턴 검사 대기
+        this.nextPattern();
+    },
+
+    patternComplete: function (){
+        this.clear();
+        this.dispatchEvent("success");
+        out("성공");
+    },
+
+    patternFailed: function (){
+        // 초기화 및 일정 시간 후 재시도 가능하도록 시간 간격을 둠
+        this.clear();
+        this.dispatchEvent("fail");
+
+        this.setRetryInterval();
+        out("실패");
+    },
+
+    dispatchEvent: function(type){
+        //dispatchEvent(new Event("success"));
+        if(type == "success"){
+            this._onSuccess();
+        }else if(type == "fail"){
+            this._onFail();
+        }
+    },
+
+    ////////////////////////////////////////////////////////////////
+    //
+    // Pattern 인식
+    //
+    ////////////////////////////////////////////////////////////////
+
+    clear: function (){
+        this.clearNextInterval();
+        this._gestureIndex = -1;
+        this._sTime = this._eTime = -1;
+    },
+
+    nextPattern: function (){
+        // 다음 패턴 검사까지 유효 시간을 체크
+        this.setNextInterval();
+    },
+
+    _nextIntervalID: -1,
+    setNextInterval: function (){
+        var self = this;
+        this.clearNextInterval();
+        this._nextIntervalID = setInterval(timeOut, this.MAX_INTERVAL);
+
+        function timeOut(){
+            self.clearNextInterval();
+            self.patternFailed();
+        }
+    },
+
+    clearNextInterval: function (){
+        if(this._nextIntervalID != -1) clearInterval(this._nextIntervalID);
+        this._nextIntervalID = -1;
+    },
+
+    _retryIntervalID: -1,
+    setRetryInterval: function (){
+        var self = this;
+        var target = this.target;
+        this.clearRetryInterval();
+        this._retryIntervalID = setInterval(timeOut, this.RETRY_INTERVAL);
+        this.deletePattern();
+
+        function timeOut(){
+            self.clearRetryInterval();
+            self.createPattern(target);
+            out("재검사 대기");
+        }
+    },
+
+    clearRetryInterval: function (){
+        if(this._retryIntervalID) clearInterval(this._retryIntervalID);
+        this._retryIntervalID = -1;
+    }
+};
+
+////////////////////////////////////////
+// Export
+////////////////////////////////////////
+
+module.exports = {
+
+};
